3c1b799de5956ec4c8ff14fcbffb359b1dac6a46
Peccheti3 <aisdevwork@gmail.com>
2025-12-26 19:43:09 +0530

    Variables example added

---
+++ 01-helloworld.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Hello world"
+echo "hi! Im super good at creating and implementation"
---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Ram : Hey How are you Rahim"
+echo "Rahim: Hi ! Ram How Are you"
+echo "Ram : Its been a long time since we ever met"
+echo "Rahim : True" 
+
