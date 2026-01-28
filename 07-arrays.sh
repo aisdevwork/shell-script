2c2cc61d5d7d56082bc23d40c19b3a3623a73c65
Peccheti3 <Iamthemaalkin@gmail.com>
2026-01-28 18:37:17 +0530

    Array example added

---
+++ 07-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MOVIES=("FEEL" "GOOD" "MOVIES" "FEELSGREAT")
+ echo "First Movie : ${MOVIES[0]}"
+ echo "Second Movie : ${MOVIES[1]}"
+ echo "Third Movie : ${MOVIES[2]}"
+  
+ echo "All Movies : ${MOVIES[@]}"
