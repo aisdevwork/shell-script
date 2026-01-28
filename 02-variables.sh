bb40b9b26140fe41c0f47ca701d6871104db478f
Peccheti3 <Iamthemaalkin@gmail.com>
2026-01-28 12:28:43 +0530

    Conversation Script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,5 +2,6 @@
 echo "Ram : Hey How are you Rahim"
 echo "Rahim: Hi ! Ram How Are you"
 echo "Ram : Its been a long time since we ever met"
-echo "Rahim : True" 
+echo "Rahim : True ! We should be meeting once in a while"
+echo "Other: Ram and Rahim together again" 
 
