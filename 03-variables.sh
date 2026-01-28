12eb7b2793014a5ed36c7534e1641abe88b6f170
Peccheti3 <Iamthemaalkin@gmail.com>
2026-01-28 12:45:59 +0530

    Conversation Variable added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+PERSON3="MR Everybody"
+
+echo "$PERSON1: Hey How are you $PERSON2"
+echo "$PERSON2: Hi !$PERSON1 How Are you"
+echo "$PERSON1 : Its been a long time since we ever met"
+echo "$PERSON2 : True ! We should be meeting once in a while"
+echo "$PERSON3: $PERSON1 and $PERSON2 together again" 
+
