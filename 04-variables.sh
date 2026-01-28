803ced4b108fb077f20412533bd21453254e7c69
Peccheti3 <Iamthemaalkin@gmail.com>
2026-01-28 14:20:38 +0530

    Conversation Arguments Added

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1: Hey How are you $PERSON2"
+echo "$PERSON2: Hi !$PERSON1 How Are you"
+echo "$PERSON1 : Its been a long time since we ever met"
+echo "$PERSON2 : True ! We should be meeting once in a while"
+echo "$PERSON3: $PERSON1 and $PERSON2 together again" 
